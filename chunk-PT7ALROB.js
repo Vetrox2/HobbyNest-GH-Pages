a9159a689caff8d163078c77be16007d70b9dfc5
Vetrox2 <mateuszmulka@spoko.pl>
2025-08-31 13:26:10 +0200

    release 3.0.0

---
+++ chunk-PT7ALROB.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-RCBZUOZR.js";import"./chunk-RQXFCEDV.js";export{a as ImgFallbackDirective};
--- chunk-RRPMRIDD.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-6YTSYAD3.js";import"./chunk-YUWHBJVV.js";export{a as ImgFallbackDirective};
